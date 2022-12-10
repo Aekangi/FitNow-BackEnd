1509b78dcc26ecb7c0d221120197d770cab9cff9
Aekangi Patel <aekangi91@gmail.com>
2022-12-09 23:57:16 -0500

    made GetAllUserDietPlanWithPk in userdietplancontroller.js

---
+++ controllers/UserDietPlanController.js
@@ -0,0 +1,21 @@
+const { DietPlan, User, UserDietPlan } = require('../models')
+const db = require('../models/index.js')
+
+const GetAllUserDietPlanWithPk = async (req, res) => {
+  try {
+    const userDietPlans = await db.sequelize.query(
+      'SELECT * FROM user_diet_plans',
+      {
+        type: db.sequelize.QueryTypes.SELECT
+      }
+    )
+
+    res.send(userDietPlans)
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  GetAllUserDietPlanWithPk
+}
