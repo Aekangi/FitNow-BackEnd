ba5c1e23ae2f37d1bb7e6acece433ed2d96f041e
Aekangi Patel <aekangi91@gmail.com>
2022-12-10 04:45:03 -0500

    updated userdietplan and userexercise seeder files

--- seeders/20221209185136-userdietplan.js
+++ seeders/20221209185136-userdietplan.js
@@ -1,22 +1,22 @@
-// 'use strict'
-// const falso = require('@ngneat/falso')
-// const { User, DietPlan } = removeEventListeneruire('../models')
+'use strict'
+const falso = require('@ngneat/falso')
+const { User, DietPlan } = require('../models')
 
-// /** @type {import('sequelize-cli').Migration} */
-// module.exports = {
-//   up: async (queryInterface, Sequelize) => {
-//     const user = await User.findAll({ raw: true })
-//     const dietPlan = await DietPlan.findAll({ raw: true })
-//     const user_diet_plans = user.map((u) => ({
-//       userId: u.id,
-//       dietPlanId: categories[Math.floor(Math.random() * dietPlan.length)].id,
-//       createdAt: falso.randPastDate(),
-//       updatedAt: falso.randRecentDate()
-//     }))
-//     await queryInterface.bulkInsert('user_diet_plans', user_diet_plans)
-//   },
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const user = await User.findAll({ raw: true })
+    const dietPlans = await DietPlan.findAll({ raw: true })
+    const user_diet_plans = user.map((u) => ({
+      userId: u.id,
+      dietPlanId: dietPlans[Math.floor(Math.random() * dietPlans.length)].id,
+      createdAt: falso.randPastDate(),
+      updatedAt: falso.randRecentDate()
+    }))
+    await queryInterface.bulkInsert('user_diet_plans', user_diet_plans)
+  },
 
-//   down: async (queryInterface, Sequelize) => {
-//     await queryInterface.bulkDelete('user_diet_plans')
-//   }
-// }
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('user_diet_plans')
+  }
+}
