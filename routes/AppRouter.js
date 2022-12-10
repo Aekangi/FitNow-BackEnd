373706d50824d87b2eea5d5c0025c2587276b811
Aekangi Patel <aekangi91@gmail.com>
2022-12-10 03:39:05 -0500

    updated approuter with user-exerciserouter

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -4,11 +4,13 @@ const UserRouter = require('./UserRouter')
 const ExerciseRouter = require('./ExerciseRouter')
 const DietPlanRouter = require('./DietPlanRouter')
 const UserDietPlanRouter = require('./UserDietPlanRouter')
+const UserExerciseRouter = require('./UserExerciseRouter')
 
 Router.use('/auth', AuthRouter)
 Router.use('/users', UserRouter)
 Router.use('/exercises', ExerciseRouter)
 Router.use('/dietplans', DietPlanRouter)
 Router.use('/user-diet-plans', UserDietPlanRouter)
+Router.use('/user-exercises', UserExerciseRouter)
 
 module.exports = Router
